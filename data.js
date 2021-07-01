41d4b76756d049ab4b10a14fef8eb7483e978a2e
patmifsud <patmifsud@me.com>
2021-07-01 14:24:27 +1000

    Now loads content including new sections from JSON objects inside data.js. Added placeholder buttons for filters. More work on styling - headings, spacing.

--- data.js
+++ data.js
@@ -5,110 +5,218 @@ const h = '<span class="highlight">';
 const hh = '</span>';
 
 
-const posts = [
-    day1 = {
-        title: "JS Terminology",
-        category: "Frontend",
-        subCategory: "Blog",
-        content: [`${h}A statement${hh} is any line that ends (or could end) in a semi colon. A funciton can also be a statement. It's kind of like sentences.`,
-            `${h}An expression${hh} is any number, string, var, equasion etc `,
-            `${h}An argument${hh} is this guy ${c}function(argument);${cc} although it's technically a parameter before getting something sent through to it. `,],
-    }
-]
 
-const codeSnippets = [
-    cssBoilerplate = {
-        title: "Css Boilerplate",
-        id:"cssBoilerplate",
-        category: "Frontend",
-        subCategory: "CSS",
-        content: [`${c}margin: 0;${cc}`,
-            `${c}outline: none;${cc}`,  
-            `${c}padding: 0;${cc}`,  
-            `${c}box-sizing: border-box;${cc}`,  
-            `${c}font-family: -apple-system, helvetica, arial, sans-serif;${cc}`, 
-            `${c}-webkit-tap-highlight-color: transparent;${cc}`,  
-            `${c}-webkit-font-smoothing: antialiased;${cc}`, 
-            `${c}-moz-osx-font-smoothing: grayscale;${cc}`],
-    },
-    terminalBasicNav = {
-        title: "Terminal - Basic Navigation",
-        id:"terminalBasicNav",
-        category: "Backend",
-        subCategory: "Terminal",
-        content: [
-         `${c}PWD${cc} - Where am I?`,
-         `${c}CD${cc} - change directory`,
-         `${c}CD-${cc} - go back`,
-         `${c}LS${cc} - list all files`,
-         `${c}LS -LA${cc} - list ${h}ALL${hh} files, including hidden ones`,
-         `${c}mkdir${cc} - make directory`,
-         `${c}touch${cc} (name), (name) - make a file or two`,
-         `${c}rm -r dreamhouse${cc} - remove something`,
-         `${c}man${cc} - manual, how to do something`,
-         `${c}wc${cc} - counts things`,
-         `${c}grep${cc} - find me something`,
-         `${c}sort${cc} - alphabetical order`,
-         `${c}curl${cc} - show me something`,
-         `${c}open${cc} - opens the thing in finder/mac os`,
-         `${c}open .${cc} - opens the current directory`,
-         `${c}|${cc} - pipe`,
-         `${c}find${cc} - shows everything in a directory and sub dirs`,
-         `${c}rm -r${cc} - remove directory`,
-         `${c}piping${cc} - send things to another program`,
-         `${c}cat${cc} - short for concatonate, opens something`,
-         `${c}atom .${cc} - open the entire directory in atom`,
-         `${c}/${cc} - jump to the top of the directory and go to a folder in there`,
-         `${c}>>${cc} - takes somethiing you entered and sends it to a file. Eg. ${c}ls >> hello.txt${cc}`  ],
-    },
-    terminalKeyboardShortcuts = {
-        title: "Terminal - Basic Navigation",
-        id:"terminalKeyboardShortcuts",
-        category: "Backend",
-        subCategory: "Terminal",
-        content: [
-            `${h}TAB key${hh} - finishes off whatever you are typing, can work on multiple folders`,
-            `${h}Option key${hh} - move insertion point with mouse`,
-        ]
-    },
-    git = {
-        title: "Git terminal",
-        id:"gitTerminal",
-        category: "Backend",
-        subCategory: "Terminal",
-        content: [
-            `${c} git add${cc} - add docs to the git`,
-            `${c} git commit${cc} - create a version of your work`,
-            `${c} git remote -v${cc} - get the gihub url`,
-            `${c} git pull upstream main ${cc} - pull changes from the main section to your local. When you push it will arrive in your fork.`,
-            `${c} git push origin main ${cc} - push your changes to your fork`,
-            `${c} git push origin main ${cc} - push your changes to your fork`,
-        ]
-    },
-    jsBasic = {
-        title: "Javascript- Things I didn't know ",
-        id:"jsBestPractice",
-        category: "Frontend",
-        subCategory: "Javascript",
-        content: [
-         c + "`The result is ${variable + 10} `" + cc + "- Backticks are better concatonation",
-        `${c}function(age=23, date = 'november', height)${cc} - default params. can also accept funcitons`,
-        `${c}&&${cc} 'and' logical operator 1> 0 && -1 > 0 = true`,
-        `${c}||${cc} 'or' logical operator 1> 0 || 0 > 0 = true`,
-        `${c}|${cc} 'not' logical operator 1 + 1 | 1 + 0 = true`,
-        `${c}(backslash)n ${cc} new line character`,
-        `${c}console.table()${cc} create a cool looking table to display an object etc`,
-        `${c}for (let key in objectName) return objectName[key];${cc} - this will return every value in an obj with key/value setup. 'in' means 'for every occurence of 'key' in 'object'`,
-        `${c}object.varWithKey${cc} doesn't work, it only works with strings. Use ${c}object[varWithKey]${cc} instead.`,
-        `${c}arguments${cc} lets you create a function with (), but take in any # or type of argument/ parametter.`,
-     ]
-    },
-]
+const content = {
+    
+    codeSnippets : [
+        html = {
+            title: "Html things I didn't know",
+            id:"html",
+            category: "Frontend",
+            subCategory: "HTML",
+            content: [
+            `Always put ${c} <!doctype html> ${cc} at the top of the html doc to avaoid quirks mode`,
+            `${c} <meta charset="utf-8"> ${cc}`]
+        },
+        cssBoilerplate = {
+            title: "Css Boilerplate",
+            id:"cssBoilerplate",
+            category: "Frontend",
+            subCategory: "CSS",
+            content: [`${c}margin: 0;${cc}`,
+                `${c}outline: none;${cc}`,  
+                `${c}padding: 0;${cc}`,  
+                `${c}box-sizing: border-box;${cc}`,  
+                `${c}font-family: -apple-system, helvetica, arial, sans-serif;${cc}`, 
+                `${c}-webkit-tap-highlight-color: transparent;${cc}`,  
+                `${c}-webkit-font-smoothing: antialiased;${cc}`, 
+                `${c}-moz-osx-font-smoothing: grayscale;${cc}`],
+        },
+        
+        terminalBasicNav = {
+            title: "Terminal - Basic Navigation",
+            id:"terminalBasicNav",
+            category: "Backend",
+            subCategory: "Terminal",
+            content: [
+            `${c}PWD${cc} - Where am I?`,
+            `${c}CD${cc} - change directory`,
+            `${c}CD-${cc} - go back`,
+            `${c}LS${cc} - list all files`,
+            `${c}LS -LA${cc} - list ${h}ALL${hh} files, including hidden ones`,
+            `${c}mkdir${cc} - make directory`,
+            `${c}touch${cc} (name), (name) - make a file or two`,
+            `${c}rm -r dreamhouse${cc} - remove something`,
+            `${c}man${cc} - manual, how to do something`,
+            `${c}wc${cc} - counts things`,
+            `${c}grep${cc} - find me something`,
+            `${c}sort${cc} - alphabetical order`,
+            `${c}curl${cc} - show me something`,
+            `${c}open${cc} - opens the thing in finder/mac os`,
+            `${c}open .${cc} - opens the current directory`,
+            `${c}|${cc} - pipe`,
+            `${c}find${cc} - shows everything in a directory and sub dirs`,
+            `${c}rm -r${cc} - remove directory`,
+            `${c}piping${cc} - send things to another program`,
+            `${c}cat${cc} - short for concatonate, opens something`,
+            `${c}atom .${cc} - open the entire directory in atom`,
+            `${c}/${cc} - jump to the top of the directory and go to a folder in there`,
+            `${c}>>${cc} - takes somethiing you entered and sends it to a file. Eg. ${c}ls >> hello.txt${cc}`,
+            `${c}curl${cc} Download a url`  ],
+        },
+        
+        terminalKeyboardShortcuts = {
+            title: "Terminal - Basic Navigation",
+            id:"terminalKeyboardShortcuts",
+            category: "Backend",
+            subCategory: "Terminal",
+            content: [
+                `${h}TAB key${hh} - finishes off whatever you are typing, can work on multiple folders`,
+                `${h}Option key${hh} - move insertion point with mouse`],
+        },
+        
+        git = {
+            title: "Git terminal",
+            id:"gitTerminal",
+            category: "Backend",
+            subCategory: "Terminal",
+            content: [
+                `${c} git add${cc} - add docs to the git`,
+                `${c} git commit${cc} - create a version of your work`,
+                `${c} git remote -v${cc} - get the gihub url`,
+                `${c} git pull upstream main ${cc} - pull changes from the main section to your local. When you push it will arrive in your fork.`,
+                `${c} git push origin main ${cc} - push your changes to your fork`,
+                `${c} git push origin main ${cc} - push your changes to your fork`,],
+        },
+        
+        jsBasic = {
+            title: "Javascript- Things I didn't know ",
+            id:"jsBestPractice",
+            category: "Frontend",
+            subCategory: "Javascript",
+            content: [
+            c + "`The result is ${variable + 10} `" + cc + "- Backticks are better concatonation",
+            `${c}function(age=23, date = 'november', height)${cc} - default params. can also accept funcitons`,
+            `${c}&&${cc} 'and' logical operator 1> 0 && -1 > 0 = true`,
+            `${c}||${cc} 'or' logical operator 1> 0 || 0 > 0 = true`,
+            `${c}|${cc} 'not' logical operator 1 + 1 | 1 + 0 = true`,
+            `${c}(backslash)n ${cc} new line character`,
+            `${c}console.table()${cc} create a cool looking table to display an object etc`,
+            `${c}for (let key in objectName) return objectName[key];${cc} - this will return every value in an obj with key/value setup. 'in' means 'for every occurence of 'key' in 'object'`,
+            `${c}object.varWithKey${cc} doesn't work, it only works with strings. Use ${c}object[varWithKey]${cc} instead.`,
+            `${c}arguments${cc} lets you create a function with (), but take in any # or type of argument/ parametter.`,
+            `${c}console.warn${cc} like console log, but with a cool yellow color.`,
+            ]
+        },
+    ],
+    
+    codeSnippets2 : [
+        html = {
+            title: "Html things I didn't know",
+            id:"html",
+            category: "Frontend",
+            subCategory: "HTML",
+            content: [
+            `Always put ${c} <!doctype html> ${cc} at the top of the html doc to avaoid quirks mode`,
+            `${c} <meta charset="utf-8"> ${cc}`]
+        },
+        cssBoilerplate = {
+            title: "Css Boilerplate",
+            id:"cssBoilerplate",
+            category: "Frontend",
+            subCategory: "CSS",
+            content: [`${c}margin: 0;${cc}`,
+                `${c}outline: none;${cc}`,  
+                `${c}padding: 0;${cc}`,  
+                `${c}box-sizing: border-box;${cc}`,  
+                `${c}font-family: -apple-system, helvetica, arial, sans-serif;${cc}`, 
+                `${c}-webkit-tap-highlight-color: transparent;${cc}`,  
+                `${c}-webkit-font-smoothing: antialiased;${cc}`, 
+                `${c}-moz-osx-font-smoothing: grayscale;${cc}`],
+        },
+        
+        terminalBasicNav = {
+            title: "Terminal - Basic Navigation",
+            id:"terminalBasicNav",
+            category: "Backend",
+            subCategory: "Terminal",
+            content: [
+            `${c}PWD${cc} - Where am I?`,
+            `${c}CD${cc} - change directory`,
+            `${c}CD-${cc} - go back`,
+            `${c}LS${cc} - list all files`,
+            `${c}LS -LA${cc} - list ${h}ALL${hh} files, including hidden ones`,
+            `${c}mkdir${cc} - make directory`,
+            `${c}touch${cc} (name), (name) - make a file or two`,
+            `${c}rm -r dreamhouse${cc} - remove something`,
+            `${c}man${cc} - manual, how to do something`,
+            `${c}wc${cc} - counts things`,
+            `${c}grep${cc} - find me something`,
+            `${c}sort${cc} - alphabetical order`,
+            `${c}curl${cc} - show me something`,
+            `${c}open${cc} - opens the thing in finder/mac os`,
+            `${c}open .${cc} - opens the current directory`,
+            `${c}|${cc} - pipe`,
+            `${c}find${cc} - shows everything in a directory and sub dirs`,
+            `${c}rm -r${cc} - remove directory`,
+            `${c}piping${cc} - send things to another program`,
+            `${c}cat${cc} - short for concatonate, opens something`,
+            `${c}atom .${cc} - open the entire directory in atom`,
+            `${c}/${cc} - jump to the top of the directory and go to a folder in there`,
+            `${c}>>${cc} - takes somethiing you entered and sends it to a file. Eg. ${c}ls >> hello.txt${cc}`,
+            `${c}curl${cc} Download a url`  ],
+        },
+        
+        terminalKeyboardShortcuts = {
+            title: "Terminal - Basic Navigation",
+            id:"terminalKeyboardShortcuts",
+            category: "Backend",
+            subCategory: "Terminal",
+            content: [
+                `${h}TAB key${hh} - finishes off whatever you are typing, can work on multiple folders`,
+                `${h}Option key${hh} - move insertion point with mouse`],
+        },
+        
+        git = {
+            title: "Git terminal",
+            id:"gitTerminal",
+            category: "Backend",
+            subCategory: "Terminal",
+            content: [
+                `${c} git add${cc} - add docs to the git`,
+                `${c} git commit${cc} - create a version of your work`,
+                `${c} git remote -v${cc} - get the gihub url`,
+                `${c} git pull upstream main ${cc} - pull changes from the main section to your local. When you push it will arrive in your fork.`,
+                `${c} git push origin main ${cc} - push your changes to your fork`,
+                `${c} git push origin main ${cc} - push your changes to your fork`,],
+        },
+        
+        jsBasic = {
+            title: "Javascript- Things I didn't know ",
+            id:"jsBestPractice",
+            category: "Frontend",
+            subCategory: "Javascript",
+            content: [
+            c + "`The result is ${variable + 10} `" + cc + "- Backticks are better concatonation",
+            `${c}function(age=23, date = 'november', height)${cc} - default params. can also accept funcitons`,
+            `${c}&&${cc} 'and' logical operator 1> 0 && -1 > 0 = true`,
+            `${c}||${cc} 'or' logical operator 1> 0 || 0 > 0 = true`,
+            `${c}|${cc} 'not' logical operator 1 + 1 | 1 + 0 = true`,
+            `${c}(backslash)n ${cc} new line character`,
+            `${c}console.table()${cc} create a cool looking table to display an object etc`,
+            `${c}for (let key in objectName) return objectName[key];${cc} - this will return every value in an obj with key/value setup. 'in' means 'for every occurence of 'key' in 'object'`,
+            `${c}object.varWithKey${cc} doesn't work, it only works with strings. Use ${c}object[varWithKey]${cc} instead.`,
+            `${c}arguments${cc} lets you create a function with (), but take in any # or type of argument/ parametter.`,
+            `${c}console.warn${cc} like console log, but with a cool yellow color.`,
+            ]
+        },
+    ],
+    
+    usefulLinks : [
+        airBnbJs = {
+            title: "Airbnb Guide to JS",
+            link: "https://github.com/airbnb/javascript",
+        }
 
-const usefulLinks = [
-    airBnbJs = {
-        title: "Airbnb Guide to JS",
-        link: "https://github.com/airbnb/javascript",
-    }
-
-]
+    ]
+}
--- script.js
+++ script.js
@@ -2,41 +2,59 @@
 
 window.onload = function(e){ 
 
-// ------------------------------------------------
-// 🚚  LOADING FROM DOM
-
-const postsWrapper = document.querySelectorAll('#posts .insert')[0];
-const codeSnippetsWrapper = document.querySelectorAll("#codeSnippets .insert")[0];
-const sections = document.querySelectorAll("section");
 
 // ------------------------------------------------
 // 🔨 PRINTING PAGE CONTENT
 
-// function to create html tags
+const mintAttr = function(attr){
+    let classStr = " ";
+    let idStr = " ";
+
+
+    for (var i = 0; i < attr.length; i++) {
+        if(attr[i].charAt(0) === "#"){
+            idStr = `id='${attr[i].slice(1)}' ` 
+        } else if(attr[i].charAt(0) === "."){
+            classStr += `${attr[i].slice(1)} ` 
+        }; 
+    }
+
+    const classFormatted = (classStr == " " ? ` ` : `class='${classStr}'`);
+    return idStr + classFormatted;
+    
+}
+
+
+// Makes html tags
 const mintHtml = function(tag, attr, content){
-    const classOrId = function(){
-        if(attr.charAt(0) === "#"){
-            return `id='${attr.slice(1)}'` }
-        else if(attr.charAt(0) === "."){
-            return `class='${attr.slice(1)}'` } 
-        return " ";
-    };     
-    return `<${tag} ${classOrId()}> ${content} </${tag}> `
+
+    let attrTags;
+    if (attr) {
+        attrTags = mintAttr(attr.split(" "))
+    }
+
+    const result = `<${tag} ${attrTags}> ${content} </${tag}> `
+    return result
+
  };
 
 
- // takes array, turns entries into string of p tags 
+ // Makes p tags from an array 
 function makePsFromArray(postContent){
+
     let p = "";
     for (let i = 0; i < postContent.length; i++) { 
         p += mintHtml("p", "", postContent[i]);
     }
   return p
+
 };
 
-// takes object, maps properties into string of formatted html 
+
+// Makes a post/card 
 function postFormatHtml(post){
-    let h2 = mintHtml("h2", "", post.title);
+
+    let h3 = mintHtml("h3", "", post.title);
     let h4 = mintHtml("h4", "", `${post.category} | ${post.subCategory}`);
     let p = "";
 
@@ -44,21 +62,97 @@ function postFormatHtml(post){
         p = makePsFromArray(post.content);
     } else p = mintHtml("p", "", post.content);
 
-    let formattedHtml = ( h2 + h4 + p )
+    let formattedHtml = ( h3 + h4 + p )
     return formattedHtml;
+
 };
 
-// takes string of html, dom location. adds html into dom location 
-function addPostsToPage(post, place){
-    let div = "";
+
+// Makes the inside of a section
+function mintPost(post){
+
+    let html = "";
+
     for (let i = 0; i < post.length; i++) { 
+        let div = "";
+
         div = mintHtml("div", ".card", postFormatHtml(post[i]));
-        place.innerHTML += div;
+        html += div;
     }
+
+    return html
 };
-    
-addPostsToPage(codeSnippets, codeSnippetsWrapper);
-addPostsToPage(posts, postsWrapper);
+
+// makes footer
+
+const footer = mintHtml("footer", ``, ' ');
+
+
+// makes array of tags for section
+
+const mintFilterTags = function(objName){
+    const obj = content[Object];
+    let filterArr = new Set();
+    let html = "";
+
+    for (Object in obj){
+        filterArr.add(obj[Object].category);
+        console.log(filterArr);
+    }
+
+    html += mintHtml("a", `.filter .active`, "All");
+    filterArr.forEach ( String => {
+        html += mintHtml("a", `.filter`, String);
+        // add logic here returning obj[Object].category; or something
+    })
+    return html;
+}
+
+
+// makes section header
+
+const mintSectionHeading = function(objName){
+    const chevron = mintHtml("div", `.chevron #c${Object}`, chev);
+    const heading = mintHtml("h2", ``, Object);
+    const filters = mintHtml("div", `.filters`, mintFilterTags(objName)); 
+    return chevron + heading + filters;
+}
+
+
+// Makes the page content
+const mintPageContent = function(){
+
+    // remove preloader
+
+    for (Object in content){
+        const obj = content[Object];
+
+        const posts = mintPost(obj);
+        const wrapper = mintHtml("div", `.wrapper`, posts);
+        const scrollContainer = mintHtml("div", `.scrollContainer`, wrapper); 
+        const headingWrapper = mintHtml("div", `.wrapper .heading`, mintSectionHeading(Object));
+        const sectionInner = headingWrapper + "\n" + scrollContainer;
+        const section = mintHtml("section", `#${Object}`, sectionInner);
+
+        document.body.innerHTML += section;
+    }
+
+    document.body.innerHTML += footer
+}
+
+
+
+
+// write page
+// mintPost(content.codeSnippets, codeSnippetsWrapper);
+// mintPost(content.posts, postsWrapper);
+mintPageContent();
+
+
+
+
+
+
 
 
 
@@ -70,6 +164,9 @@ const openSection = function(){
 }
 
 
+const sections = document.querySelectorAll("section");
+
+
 // temp adding open on click to the sections
 for (var i = 0; i < sections.length; i++) {
     sections[i].onclick = openSection;
@@ -79,3 +176,9 @@ for (var i = 0; i < sections.length; i++) {
 }
 
 
+
+
+// misc
+const chev = `<svg width="15" height="9" viewBox="0 0 15 9" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M1.25781 6.51562C1.08594 6.67969 0.992188 6.89844 0.992188 7.14844C0.992188 7.65625 1.39062 8.05469 1.89844 8.05469C2.14844 8.05469 2.38281 7.96094 2.55469 7.78125L7.91406 2.28906L13.2734 7.78125C13.4453 7.96094 13.6797 8.05469 13.9297 8.05469C14.4297 8.05469 14.8281 7.65625 14.8281 7.14844C14.8281 6.89844 14.7422 6.67969 14.5703 6.51562L8.61719 0.421875C8.42188 0.21875 8.17969 0.109375 7.91406 0.109375C7.64844 0.109375 7.41406 0.210938 7.21094 0.421875L1.25781 6.51562Z" fill="black"/>
+</svg>`;
